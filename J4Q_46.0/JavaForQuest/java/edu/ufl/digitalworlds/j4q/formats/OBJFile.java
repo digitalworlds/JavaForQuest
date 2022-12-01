fc149ab87f53694c563dce6f7a66b62afb5c1ca9
Digital Worlds Institute <angelos@digitalworlds.ufl.edu>
2022-12-01 13:56:25 -0500

    OBJFile was created

---
+++ J4Q_46.0/JavaForQuest/java/edu/ufl/digitalworlds/j4q/formats/OBJFile.java
@@ -0,0 +1,18 @@
+package edu.ufl.digitalworlds.j4q.formats;
+
+public class OBJFile {
+
+    public OBJFile(String filename){
+
+        //Open a line by line file readed in Java
+        //parse the obj text file
+
+        //run the "parse" code
+
+        //This will create the array lists: XYZ, Normals, UV, trinagles
+
+        //run the postProcessing code
+
+    }
+
+}
