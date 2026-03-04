726c35510b658c3b62d5408bedf582326c193528
Angelos Barmpoutis <angelbar@ufl.edu>
2026-03-04 02:12:02 -0500

    J4Q_85 restructured

--- J4Q_85.0/Samples/XrSamples/XrCompositor_NativeActivity/assets/shaders/colors.frag
+++
@@ -1,9 +0,0 @@
-#version 300 es
-precision mediump float;
-in lowp vec4 vColor;
-out lowp vec4 outColor;
-
-void main()
-{
-    outColor = vColor;
-}
--- J4Q_85.0/Samples/XrSamples/XrCompositor_NativeActivity/java/gl/controllers/J4QToggleButton.java
+++
@@ -1,9 +0,0 @@
-package gl.controllers;
-
-public class J4QToggleButton {
-    public boolean changedSinceLastSync=false;
-    public boolean currentState=false;
-    public boolean isPressed(){return currentState;}
-    public boolean justPressed(){return changedSinceLastSync && currentState;}
-    public boolean justReleased(){return changedSinceLastSync && !currentState;}
-}
